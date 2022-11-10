08d4cc88debfa47683dcc949d37a2d27d55200e3
GtDavide01 <davidefiorini01@gmail.com>
2022-11-10 14:21:03 +0100

    scaffholding progetto concluso

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<!-- Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Al caricamento della pagina, effettuate una chiama ajax all'API di Breaking Bad:
+https://www.breakingbadapi.com/api/characters
+e con i dati restituiti, stampate una card per ogni personaggio. Bonus:
+Creare un componente loader da visualizzare fintantoché i risultati non sono pronti.-->
+<!-- SCRIPT -->
+<script>
+// IMPORT
+import AppHeaderVue from "./components/AppHeader.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    AppHeaderVue,
+  },
+};
 </script>
-
+<!-- HTML -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeaderVue />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<!-- CSS/SCSS -->
+<style lang="scss">
+@use "./styles/general.scss";
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <h1>Breaking Bad Api</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+// HEADER
+header {
+  height: 100px;
+  h1 {
+    color: white;
+    text-align: center;
+    font-size: 3rem;
+    padding-top: 2rem;
+  }
+}
+</style>
